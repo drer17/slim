87bf610f287231880c7eb1d2955fb760fc3f71b2
drer17 <79947155+drer17@users.noreply.github.com>
2024-09-22 09:25:04 +0930

    Initial commit

---
+++ app/theme/fonts.ts
@@ -0,0 +1,10 @@
+import { Roboto } from "next/font/google";
+
+/**
+ * The Roboto font configuration.
+ */
+export const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
