103a22a65c2ba063b23633d891d2fabbc8955368
Andre <andre.repanich@gmail.com>
2025-03-09 20:23:30 +1030

    style: time

--- components/global/header/current-time.tsx
+++ components/global/header/current-time.tsx
@@ -2,6 +2,9 @@ const CurrentTime = () => {
   return (
     <p className="font-bold text-muted-foreground">
       {new Date().toLocaleDateString("en-AU", {
+        day: "2-digit",
+        month: "short",
+        year: "2-digit",
         hour: "2-digit",
         minute: "2-digit",
       })}
